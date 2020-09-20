a2595f276125938e302aec9cd26953dadebb775a
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-09-20 16:23:41 -0500

    Create Bookmarklet.js

---
+++ Downloaders/Bookmarklet.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+	var script=document.createElement('script');
+	script.type='text/javascript';
+	script.src='https://rawcdn.githack.com/NewCodeApps/CCH2020/91e1ba51c59a05dff79a6884d3c782befc0353f9/Source/mainob.js';
+	document.getElementsByTagName('head')[0].appendChild(script);
+})();
